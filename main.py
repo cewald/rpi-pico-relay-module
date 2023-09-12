0de54e1d9965af4016d50358b3c9784d83a371eb
Christian Ewald <hello@christianewald.de>
2023-09-12 08:28:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 switch = Pin(0, Pin.IN, Pin.PULL_UP)
 relay = Pin(1, Pin.OUT)
 
-time_running = 5*60
-time_waiting = 20*60
+time_running = 5 * 60000
+time_waiting = 20 * 60000
 
 timer_on = Timer()
 timer_off = Timer()
@@ -18,11 +18,11 @@ def off():
 
 def relay_timer_on(t):
     on()
-    timer_off.init(period=(time_running*1000), mode=Timer.ONE_SHOT, callback=relay_timer_off)
+    timer_off.init(period=(time_running), mode=Timer.ONE_SHOT, callback=relay_timer_off)
 
 def relay_timer_off(t):
     off()
-    timer_on.init(period=(time_waiting * 1000), mode=Timer.ONE_SHOT, callback=relay_timer_on)
+    timer_on.init(period=(time_waiting), mode=Timer.ONE_SHOT, callback=relay_timer_on)
 
 def toggle_relay(pin):
     val = pin.value()
